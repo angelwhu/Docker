108c5880ef6f47870b0e64516fd7d9f3c25a9db9
angelwhu <huanqiwhu@163.com>
2016-04-08 22:15:38 +0800

    add nginx-web files

---
+++ docker-compose-nginx-web/web/webcode/test_db.php
@@ -0,0 +1,19 @@
+<?php
+$con = mysql_connect("mysql_db","root","angelwhu123");
+if (!$con)
+{
+    die('Could not connect: ' . mysql_error());
+}
+
+// some code
+
+mysql_select_db("test_db", $con);
+$result = mysql_query("SELECT * FROM student");
+
+while($row = mysql_fetch_array($result))
+{
+    echo $row['name'] . " " . $row['age'];
+    echo "<br />";
+}
+mysql_close($con);
+?>
